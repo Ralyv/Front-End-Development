d5170f5dca8221f0fb019e974c38e0843d8c842f
ralyv <QA_2015@mail.bg>
2016-07-23 16:14:08 +0300

    L13_Homework_04

---
+++ L13_Homework_04/L13_Homework_04.js
@@ -0,0 +1,16 @@
+/*Напишете скрипт, който прихваща click събитие на даден елемент и fadeIn-ва друг.*/
+
+$(document).ready(function() {
+	
+	var btnFadeOut = $('#fadeOut')[0];
+	
+	var bulbFade = $('#imageFade'); 
+	
+	var imgFadeOut = function(){
+		$(bulbFade).fadeOut(3000);
+	}
+	
+	$(btnFadeOut).click(imgFadeOut);
+});
+
+
