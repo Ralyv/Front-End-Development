accae588e82f0b444fded7133dd1c0b261b53e7e
ralyv <QA_2015@mail.bg>
2016-07-27 00:25:25 +0300

    L14_Homework

---
+++ L14_Homework/L14_Homework.js
@@ -0,0 +1,32 @@
+/*Създайте таблица с цветове, както във файла task1.png.
+Данните от таблицата трябва да се зареждат асинхронно при отваряне на страницата, посредством AJAX.
+Използвайте файла data.json
+*Всеки ред трябва да има зададен background, отговарящ на стойността на hexValue*/
+
+
+var tableBody = $('tbody');
+
+$.ajax({
+	method: "GET",
+	url: "data/data.json",
+	dataType: "json"
+}).done(function(data){
+
+	var datacolorsArray = data.colorsArray;
+
+	datacolorsArray.forEach(function(product){
+		var tr = $("<tr><td>" + product.colorName + "</td>" + "<td>" + product.hexValue + "</td></tr>");
+
+		tableBody.append(tr);
+
+		$('td').css({border: '1px solid black'});
+
+		$(tr).css({'backgroundColor': product.hexValue});
+	});
+	
+}).fail(function(error){
+	alert("Data can not be loaded")
+	console.log(error);
+});
+
+
